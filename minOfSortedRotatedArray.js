0822fc3376695bf6067ce8ad825a21f6930156a1
Progoogler <Andyzanli@gmail.com>
2016-04-10 22:29:20 -0700

    Find min of a sorted and rotated array

--- minOfSortedRotatedArray.js
+++ minOfSortedRotatedArray.js
@@ -3,28 +3,31 @@
 * unknown point. This can be achieved in O(log n) time using Binary Search.
 */
 
-"use strict";
-const findMin = (array, start = 0, end = array.length-1) => {
+(function(){
+  "use strict";
 
-  if (start === end) return array[start];
+  const findMin = (array, start = 0, end = array.length-1) => {
 
-  let mid = end + Math.floor((start - end) / 2);
+    if (start === end) return array[start];
+    if (end === -1) return;
 
-  if (mid < end && array[mid+1] < array[mid]) {
-  	return array[mid+1];
-  }
+    let mid = Math.floor((start + end) / 2);
 
-  if (mid > start && array[mid] < array[mid-1]) {
-  	return array[mid];
-  }
-  
-  if (array[start] > array[mid]) {
-  	return findMin(array, start, mid-1);
-  } else {
-  	return findMin(array, mid+1, end);
-  }
+    if (mid < end && array[mid+1] < array[mid]) {
+    	return array[mid+1];
+    }
 
-  return -1;
-};
+    if (mid > start && array[mid] < array[mid-1]) {
+    	return array[mid];
+    }
+    
+    if (array[start] > array[mid]) {
+    	return findMin(array, start, mid-1);
+    } else {
+    	return findMin(array, mid+1, end);
+    }
 
-findMin([5, 6, 7, 1, 2, 3, 4]);
+    return -1;
+  };
+
+})();
